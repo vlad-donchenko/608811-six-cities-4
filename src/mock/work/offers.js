13f09123576cf3e52d5781fc31031fa4b30180f1
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-06-22 12:22:51 +0300

    create mock

---
+++ src/mock/work/offers.js
@@ -0,0 +1,49 @@
+const offers = [
+  {
+    title: `Beautiful & luxurious apartment at great location`,
+    picture: `img/apartment-01.jpg`,
+    price: 120,
+    type: `Apartment`,
+    rating: 4,
+    isPremium: true,
+    isBookmark: true
+  },
+  {
+    title: `Wood and stone place`,
+    picture: `img/apartment-01.jpg`,
+    price: 220,
+    type: `Apartment`,
+    rating: 3,
+    isPremium: false,
+    isBookmark: false
+  },
+  {
+    title: `Canal View Prinsengracht`,
+    picture: `img/apartment-01.jpg`,
+    price: 150,
+    type: `Apartment`,
+    rating: 2,
+    isPremium: false,
+    isBookmark: false
+  },
+  {
+    title: `Nice, cozy, warm big bed apartment`,
+    picture: `img/apartment-01.jpg`,
+    price: 850,
+    type: `Apartment`,
+    rating: 5,
+    isPremium: false,
+    isBookmark: true
+  },
+  {
+    title: `Wood and stone place`,
+    picture: `img/apartment-01.jpg`,
+    price: 450,
+    type: `Apartment`,
+    rating: 4.5,
+    isPremium: true,
+    isBookmark: false
+  }
+];
+
+export {offers};
