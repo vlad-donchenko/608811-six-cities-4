cc7d096106316f0fab445a52ea31e5bfe545b90b
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-07-06 20:14:18 +0300

    create data for reviews components, const, utils and finished reviews components

---
+++ src/const.js
@@ -0,0 +1,3 @@
+const MONTH = [`January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`];
+
+export {MONTH};
---
+++ src/utils/index.js
@@ -0,0 +1,24 @@
+import {MONTH} from "../const";
+
+const getFormattedInUserDate = (date) => {
+  const currentDate = new Date(date);
+  const month = MONTH[currentDate.getMonth()];
+  const year = currentDate.getFullYear();
+
+  return `${month} ${year}`
+};
+
+const getFormattedInDateTime = (date) => {
+  const currentDate = new Date(date);
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth() < 10 ? `0${currentDate.getMonth()}` : currentDate.getMonth();
+  const year = currentDate.getFullYear() < 10 ? `0${currentDate.getFullYear()}` : currentDate.getFullYear();
+
+  return `${year}-${month}-${day}`
+}
+
+const convertRatingInSvgWidth = (userRating) => {
+  return `${userRating * 10 * 2}%`
+};
+
+export {getFormattedInUserDate, getFormattedInDateTime, convertRatingInSvgWidth};
