6eaa0a02f25a57be8b4c3f641bca8af5dcdacaed
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-07-06 17:34:47 +0300

    create Review list component

---
+++ src/components/review-list/review-list.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ReviewsList = () => {
+  return (
+    <section className="property__reviews reviews">
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
+      <ul className="reviews__list">
+
+      </ul>
+    </section>
+);
+};
+
+export default ReviewsList;
